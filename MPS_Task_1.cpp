b9225bc608ce7be6552b2b87158ad110cb547676
EeshaSharma <gulabi.usa@gmail.com>
2018-07-02 23:21:54 -0400

    second change

--- MPS_Task_1.cpp
+++ MPS_Task_1.cpp
@@ -12,10 +12,30 @@
 using namespace std;
 
 
+void fun1(){
+	int val = 10000;
+	//Sleep(2);
+	for (int i = 0; i<10000; i++){
+		val += 100000;
+	}
+}
+
+double timefind(int NumOfIterations){
+	clock_t begin = clock();
+	for (int i = 0; i < NumOfIterations; i++){
+		fun1();
+	}
+	clock_t end = clock();
+	double timeElapsed = double(end - begin) / CLOCKS_PER_SEC;
+	return timeElapsed;
+}
 
 int main()
 {
-	cout << "MPS_Task_1\n";
+	cout << "Hello World\n";
 
+	cout << timefind(10000000) << " seconds" << endl;
+
+	// double result = timesRun(times, &fun1);
 	return 0;
 }
